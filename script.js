e754895d352a6f8a01ad7999fc946f5f0aaad62b
Willemina <wstikk@gmail.com>
2016-03-17 14:19:07 -0600

    Added files via upload
    First upload (via web since terminal attempt failed)

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	console.log("Script included!");	
+});
+
+$(document).ready(function () {
+     $('<input type="checkbox" value="1" />').prependTo('li');
+ });
+
+$(document).ready(function(){
+	$('img').click(function(){
+		$('img').toggle();
+	});
+});
+
